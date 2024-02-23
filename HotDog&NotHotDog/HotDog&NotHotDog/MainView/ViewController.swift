e85cbda8af16da4e3d8a396181f11e261e55ece9
kauamiguel <kauamcm@gmail.com>
2024-02-23 07:32:45 -0300

    Colocando os elementos no arquivo View

--- HotDog&NotHotDog/HotDog&NotHotDog/MainView/ViewController.swift
+++ HotDog&NotHotDog/HotDog&NotHotDog/MainView/ViewController.swift
@@ -9,83 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var takePictureButton : UIButton = {
-        let button = UIButton(type: .system)
-        button.setTitle("Take Picture", for: .normal)
-        button.setTitleColor(.white, for: .normal)
-        button.frame = CGRect(x: 0, y: 0, width: 150, height: 100)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.backgroundColor = .systemBlue
-        button.layer.zPosition = 2
-        return button
-    }()
-    
-    var imageView : UIImageView = {
-        let imgView = UIImageView()
-        imgView.translatesAutoresizingMaskIntoConstraints = false
-        imgView.layer.zPosition = 1
-        return imgView
-    }()
-    
-    var hotDog : UIImageView = {
-        let image = UIImageView(image: UIImage(named: "hotDogImage"))
-        image.translatesAutoresizingMaskIntoConstraints = false
-        image.layer.zPosition = 4
-        return image
-    }()
-    
-    var notHotDog : UIImageView = {
-        let image = UIImageView(image: UIImage(named: "notHotDogImage"))
-        image.translatesAutoresizingMaskIntoConstraints = false
-        image.layer.zPosition = 4
-        return image
-    }()
+    let viewReference = View()
     
     var model = HotDogModel()
     
     var isHotDog : Bool = false
     
-    var image : UIImage = UIImage()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        takePictureButton.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
-        setupView()
+        viewReference.setupView(view: self.view)
+        viewReference.takePictureButton.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
     }
     
     
     @objc func didTapButton(){
         
         //Remove the view of result of the scene
-        hotDog.removeFromSuperview()
-        notHotDog.removeFromSuperview()
+        viewReference.hotDogFeedBack.removeFromSuperview()
+        viewReference.notHotDogFeedBack.removeFromSuperview()
         
         let picker = UIImagePickerController()
         picker.sourceType = .camera
         picker.delegate = self
         present(picker, animated: true)
     }
-    
-    func setupView(){
-        self.view.backgroundColor = .white
-        
-        view.addSubview(imageView)
-        view.addSubview(takePictureButton)
-        
-        takePictureButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        takePictureButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
-        
-        imageView.widthAnchor.constraint(equalToConstant: 350).isActive = true
-        imageView.heightAnchor.constraint(equalToConstant: 600).isActive = true
-        imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -30).isActive = true
-        imageView.trailingAnchor.constraint(equalTo : view.trailingAnchor, constant: -20).isActive = true
-        imageView.bottomAnchor.constraint(equalTo: takePictureButton.topAnchor, constant: -10).isActive = true
-        
-        imageView.backgroundColor = .gray
-    }
-    
     
 }
 
@@ -104,44 +52,42 @@ extension ViewController : UIImagePickerControllerDelegate, UINavigationControll
         //get the image taken
         guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else { return }
         
-        self.image = image
-        let result = coreMLResult(forImage: image)
+        guard let result = coreMLResult(forImage: image) else {return}
         if result == "hotdog"{
             self.isHotDog = true
         }else{
             self.isHotDog = false
         }
         displayResult()
-        imageView.image = image
+        viewReference.imageView.image = image
     }
     
-    func coreMLResult(forImage image : UIImage) -> String{
+    func coreMLResult(forImage image : UIImage) -> String?{
         
         if let pixelImage = ImageProcessor.pixelBuffer(forImage: image.cgImage!){
             guard let scene = try? model.prediction(image: pixelImage) else {fatalError()}
             return scene.target
         }
         
-        return ""
+        return nil
     }
     
     func displayResult(){
         if isHotDog{
-            self.view.addSubview(hotDog)
-            hotDog.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-            hotDog.widthAnchor.constraint(equalToConstant: 350).isActive = true
-            hotDog.heightAnchor.constraint(equalToConstant: 150).isActive = true
-            hotDog.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+            self.view.addSubview(viewReference.hotDogFeedBack)
+            viewReference.hotDogFeedBack.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+            viewReference.hotDogFeedBack.widthAnchor.constraint(equalToConstant: 350).isActive = true
+            viewReference.hotDogFeedBack.heightAnchor.constraint(equalToConstant: 150).isActive = true
+            viewReference.hotDogFeedBack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
             
         }else{
-            self.view.addSubview(notHotDog)
-            notHotDog.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-            notHotDog.widthAnchor.constraint(equalToConstant: 350).isActive = true
-            notHotDog.heightAnchor.constraint(equalToConstant: 150).isActive = true
-            notHotDog.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+            self.view.addSubview(viewReference.notHotDogFeedBack)
+            viewReference.notHotDogFeedBack.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+            viewReference.notHotDogFeedBack.widthAnchor.constraint(equalToConstant: 350).isActive = true
+            viewReference.notHotDogFeedBack.heightAnchor.constraint(equalToConstant: 150).isActive = true
+            viewReference.notHotDogFeedBack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
         }
         
-        view.bringSubviewToFront(takePictureButton)
     }
     
 }
